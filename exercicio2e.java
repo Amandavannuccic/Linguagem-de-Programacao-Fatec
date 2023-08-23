8b60d96dfc5ae39d6435a0810624c12e7d5b47c9
Amanda Vannucci <127263243+Amandavannuccic@users.noreply.github.com>
2023-08-22 22:14:21 -0300

    Create exercicio2e.java
    2e

---
+++ exercicio2e.java
@@ -0,0 +1,24 @@
+public class TrocaVariaveis {
+    public static void main(String[] args) {
+        // Inicialize A com 10 e B com 5
+        int A = 10;
+        int B = 5;
+        
+        // Calcule C como a soma de A e B
+        int C = A + B;
+        
+        // Atualize B para 20 e A para 10
+        B = 20;
+        A = 10;
+        
+        // Imprima os valores de A, B e C
+        System.out.println("A: " + A);
+        System.out.println("B: " + B);
+        System.out.println("C: " + C);
+    }
+}
+Neste código, inicializamos A com 10 e B com 5, calculamos C como a soma de A e
+
+A: 10
+B: 20
+C: 15
